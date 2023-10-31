cfe094875469b5706860090c5d3be4b20f21c47c
farhatbaig <farhatbaig77@gmail.com>
2023-10-31 21:07:59 +0500

    code

--- const.js
+++ const.js
@@ -1,2 +1,2 @@
-export const apiKey  = 'YIO7Wio7uc8kng2OQd5pvALrTQqs0rRA';
-export const apiUrl = 'https://api.nytimes.com/svc/topstories/v2/arts.json'
+ const apiKey  = 'YIO7Wio7uc8kng2OQd5pvALrTQqs0rRA';
+ const apiUrl = 'https://api.nytimes.com/svc/topstories/v2/arts.json'
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
-
+const apiKey  = 'YIO7Wio7uc8kng2OQd5pvALrTQqs0rRA';
+const apiUrl = 'https://api.nytimes.com/svc/topstories/v2/arts.json'
 // Use CORS middleware
 app.use(cors({
     origin: 'http://localhost:3000' 
@@ -13,14 +14,15 @@ app.listen(3001, () => {
 });
 
 app.get('/getStories', async (req, res) => {
+  
   const stories = await axios.get(`${apiUrl}?api-key=${apiKey}`);
+  console.log('stories',stories);
   res.send(stories.data);
 });
 app.get('/getStories/:index', async (req, res) => {
   const index = req.params.index;
-  const stories = await axios.get('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=YIO7Wio7uc8kng2OQd5pvALrTQqs0rRA');
+  const stories = await axios.get(`${apiUrl}?api-key=${apiKey}`);
   const story = stories.data.results[index];
-  console.log('data',story);
   if (story) {
     res.send(story);
   } else {
